ce78ee37a3f5eddc750b171cc136af2e00739b8a
Martin-Junior34 <86049270+Martin-Junior34@users.noreply.github.com>
2022-06-29 04:57:59 +0000

    Add files via upload

---
+++ models/userCL.dart
@@ -0,0 +1,15 @@
+
+class UserCL {
+  String? fullName;
+  String? email;
+  String? phone;
+  String? id;
+
+  UserCL({this.id, this.phone, this.email, this.fullName});
+  UserCL.fromSnapshot(snapshot) {
+    id = snapshot.key;
+    phone = snapshot.value['phone'];
+    email = snapshot.value['email'];
+    fullName = snapshot.value['fullname'];
+  }
+}
