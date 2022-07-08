4c316ce3ac135771fefdfa8f6af51ae239567871
Martin-Junior34 <86049270+Martin-Junior34@users.noreply.github.com>
2022-07-08 08:32:30 +0000

    Delete Screens directory

--- Screens/screens.dart
+++
@@ -1,4 +0,0 @@
-export 'homescreen.dart';
-export 'send_parcel_screen.dart';
-export 'send_parcel_detail_screen.dart';
-export 'send_parcel_checkout_screen.dart';
--- Screens/widgets/BrandDivider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BrandDivider extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return
-      Divider(height: 1, color: Color(0xffe2e2e2),thickness: 1,);
-  }
-}
