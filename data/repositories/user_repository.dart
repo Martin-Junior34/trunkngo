c82076659178d3b5dbdf28b18cc2fc9aa82b87fe
Martin-Junior34 <86049270+Martin-Junior34@users.noreply.github.com>
2022-07-08 08:37:24 +0000

    Delete data/repositories directory

--- data/repositories/user_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:async';
-import 'package:truckngo/models/models.dart';
-
-class UserRepository {
-  final StreamController<User> _streamController = StreamController<User>();
-  Stream<User> get apiUser => _streamController.stream;
-}
